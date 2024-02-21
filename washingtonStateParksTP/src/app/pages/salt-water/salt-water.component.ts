c78d204bced47c4ea09e74d5ee38fcbe4b7c0d3f
jbackus1 <Joshua.backus@outlook.com>
2024-02-21 13:01:50 -0800

    Completed
    Made two more site pages and working on information

---
+++ washingtonStateParksTP/src/app/pages/salt-water/salt-water.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-salt-water',
+  standalone: true,
+  imports: [],
+  templateUrl: './salt-water.component.html',
+  styleUrl: './salt-water.component.css'
+})
+export class SaltWaterComponent {
+
+}
